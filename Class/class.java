f9aed7a88f1139cf9148e0593fb8b1cbdf99a15b
Munna <mygames.x20@gmail.com>
2023-09-02 16:17:55 +0600

    Code

--- Class/class.java
+++ Class/class.java
@@ -1,1 +1,29 @@
-hl
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Student munna = new Student(13,"Munna",3.85f);
+        System.out.println(munna.roll);
+        System.out.println(munna.name);
+        System.out.println(munna.marks);
+    }
+}
+
+
+// Primitives: int, float, char are stored in stack memory
+// objects: objects are stored in heap memory
+// final (keyword): always assign while declaring
+
+
+class Student{
+    int roll;
+    String name;
+    float marks;
+
+
+    // Create constructor
+    Student(int roll, String name, float marks){
+        this.roll = roll;
+        this.name = "name";
+        this.marks = marks;
+    } // constructor is called automatically
+
