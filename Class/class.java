711f1639709eec3c34e25a0812bfdb19622856c9
Munna <mygames.x20@gmail.com>
2023-09-10 19:10:06 +0600

    Code

--- Class/class.java
+++ Class/class.java
@@ -1,29 +1,35 @@
-import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        Student munna = new Student(13,"Munna",3.85f);
-        System.out.println(munna.roll);
-        System.out.println(munna.name);
-        System.out.println(munna.marks);
+        Student s = new Student();
+
+        s.roll = 15;
+        s.name = "Munna";
+        s.cgpa = 3.95f;
+
+        System.out.println(s.roll);
+        System.out.println(s.name);
+        System.out.println(s.cgpa);
+
     }
 }
-
-
-// Primitives: int, float, char are stored in stack memory
-// objects: objects are stored in heap memory
-// final (keyword): always assign while declaring
 
 
 class Student{
     int roll;
     String name;
-    float marks;
+    float cgpa;
+};
 
 
-    // Create constructor
-    Student(int roll, String name, float marks){
-        this.roll = roll;
-        this.name = "name";
-        this.marks = marks;
-    } // constructor is called automatically
+/* Class : Class is a template of an object unlike physical. Class creates a data type that you can use to create object.
+Class is just a logical constructor */
 
+/* Object: Object is an  instance of a class, when you declare an object of a class you are creating an instance of a class.
+Object occupies space in memory unlike class */
+
+
+// Primitives: int, float, char are stored in stack memory
+// Object: Objects are stored in (heap memory)
+// Reference: References are stored in (stack memory)
+// final (keyword): Always assign while declaring
